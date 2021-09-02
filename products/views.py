44c9fd6ade0df7a81c71b99c89984932d88913a7
StopGuard <stopguard@bk.ru>
2021-09-02 23:34:29 +0500

    HW2 updated

--- products/views.py
+++ products/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
+from datetime import datetime
 
 
 # Create your views here.
 # Controller-functions
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'page_title': 'geekshop',
+        'today': datetime.now(),
+    }
+    return render(request, 'index.html', context)
 
 
 def products(request):
-    return render(request, 'products.html')
+    context = {
+        'page_title': 'geekshop - каталог',
+        'today': datetime.now(),
+    }
+    return render(request, 'products.html', context)
