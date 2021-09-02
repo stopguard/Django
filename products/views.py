4fb87089f39ce5b000b68cba286cf4d9de79690f
StopGuard <stopguard@bk.ru>
2021-09-02 22:03:06 +0500

    HW2 added

--- products/views.py
+++ products/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 # Controller-functions
 def index(request):
     return render(request, 'index.html')
+
 
 def products(request):
     return render(request, 'products.html')
