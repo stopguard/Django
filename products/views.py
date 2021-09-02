a0c2156e969e8d8f64c5f952a6d3490ef7ab929e
StopGuard <stopguard@bk.ru>
2021-09-03 01:01:59 +0500

    HW2 final version

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from datetime import datetime
+from json import load
 
 
 # Create your views here.
@@ -16,5 +17,12 @@ def products(request):
     context = {
         'page_title': 'geekshop - каталог',
         'today': datetime.now(),
+        'products': extract_data('db.json')
     }
     return render(request, 'products.html', context)
+
+
+def extract_data(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        data = load(f)
+    return data
