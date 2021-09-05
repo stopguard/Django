6b253aef4d132f2f37697f9cb14c2f6460f47aae
StopGuard <stopguard@bk.ru>
2021-09-05 19:01:52 +0500

    HW3 added, add media url and settings, move product images, add models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class ProductCategories(models.Model):
+    name = models.CharField('Категория', max_length=64)
+    description = models.CharField('Описание', max_length=256, blank=True)
+
+
+class Products(models.Model):
+    category = models.ForeignKey(ProductCategories, on_delete=models.CASCADE)
+    name = models.CharField('Название', max_length=64)
+    image = models.ImageField(upload_to='product_images', blank=True)
+    description = models.CharField('Описание', max_length=256, blank=True)
+    price = models.DecimalField('Цена', max_digits=9, decimal_places=2, default=0)
+    quantity = models.IntegerField('Количество на складе', default=0)
