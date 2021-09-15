388765f9eee8acfc296fa81f18127d31cda10309
stopguard <stopguard@bk.ru>
2021-09-15 21:16:19 +0500

    add all answers

---
+++ authapp/forms.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.forms import forms
+
+from authapp.models import ShopUser
+
+
+class ShopUserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = f'form-control py-4'
+            field.widget.attrs['placeholder'] = field_name.title()
+
+
+class ShopUserRegisterForm(UserCreationForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'first_name', 'last_name', 'password1', 'password2', 'email', 'avatar')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+            field.help_text = ''
+
+    def clean_username(self):
+        u_name = self.cleaned_data['username']
+        if len(u_name) < 6:
+            raise forms.ValidationError('Длина имени должна быть не меньше 6 символов')
+        return u_name
+
--- authapp/management/commands/make_su.py
+++ authapp/management/commands/make_su.py
@@ -7,4 +7,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not ShopUser.objects.filter(username='django').exists():
             ShopUser.objects.create_superuser('django', '', 'geekshop')
-        print('Standard super user created')
+            print('Standard super user created')
+        else:
+            print('Standard user is exists')
