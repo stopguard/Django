248c19f90da5e9784bbd3133355139f5f921a45f
stopguard <stopguard@bk.ru>
2021-12-03 03:30:12 +0500

    add smoke-tests and login/login-redirect tests

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.test.client import Client
+from django.urls import reverse
 
-# Create your tests here.
+from products.models import ProductsCategory
+
+
+class TestProductsSmoke(TestCase):
+    fixtures = ['products.json']
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.client = Client()
+
+    def test_product_urls(self):
+        response = self.client.get(reverse('products:index'))
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(reverse('products:products'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_category_urls(self):
+        response = self.client.get(reverse('products:category',
+                                           kwargs={'cat_id': 0}))
+        self.assertEqual(response.status_code, 200)
+
+        for category in ProductsCategory.objects.filter(is_active=True):
+            response = self.client.get(reverse('products:category', kwargs={'cat_id': category.pk}))
+            self.assertEqual(response.status_code, 200)
